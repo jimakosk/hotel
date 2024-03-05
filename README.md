6e8832bff8434db0bf8276f13d34aee466e310a4
jimakosk <jkon2@DESKTOP-FEKS7V2>
2024-03-05 22:58:55 +0200

    com

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Hotelapl
 
+#ng build  --base-href /Users/Angular/hotelapl/dist/hotelapl/browser/
+ng build  --base-href ./
--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-HS4L6FXX.mjs";import"./chunk-STRHAXKI.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-YU7FE4TF.mjs";import"./chunk-STRHAXKI.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
